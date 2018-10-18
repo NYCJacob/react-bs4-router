3e3ac39f4983888706bb34e9ef2d0cccda3b21ae
Jacob Sherman <jacob@jbsherman.com>
2018-10-18 16:50:22 -0400

    fix: tab nav styling almost as in bootstrap except bottom border only goes as far as links rather than div width

--- src/components/RouterTabs.js
+++ src/components/RouterTabs.js
@@ -18,31 +18,42 @@ export function RouterTabs(props) {
 }
 
 // this is hardcoding values following egghead tutorial
+
+
+const  tabStyles = {
+    borderColor: "#fff #fff #dee2e6"
+};
+
+const activeStyles = {
+    fontWeight: "bold",
+    borderColor: "#dee2e6 #dee2e6 #fff"
+};
+
 export const RouterLinks = () => (
     <div className="col">
-        <ul className="siteNav nav nav-tabs">
+        <ul className="siteNav nav nav-tabs" style={ {borderBottom: "#fff" } }>
             <li className="nav-item">
-                <NavLink exact activeStyle={ {backgroundColor: "#40617F", fontWeight: "bold"}} className="nav-link" to="/Zoning">
+                <NavLink exact activeStyle={ { fontWeight: "bold", borderColor: "#dee2e6 #dee2e6 #fff" }} style={ tabStyles } className="nav-link" to="/Zoning">
                     Zoning
                 </NavLink>
             </li>
             <li className="nav-item">
-                <NavLink activeStyle={ {backgroundColor: "#40617F"}} className="nav-link" to="/Census">
+                <NavLink activeStyle={ activeStyles}  style={ tabStyles } className="nav-link" to="/Census">
                     Census
                 </NavLink>
             </li>
             <li className="nav-item">
-                <NavLink exact activeClassName="active" className="nav-link" to="/TownBoard">
+                <NavLink exact activeClassName="active"  style={ tabStyles } className="nav-link" to="/TownBoard">
                     Town Board
                 </NavLink>
             </li>
             <li className="nav-item">
-                <NavLink exact activeClassName="active" className="nav-link" to="/EventLaw">
+                <NavLink exact activeClassName="active"  style={ tabStyles } className="nav-link" to="/EventLaw">
                     Event Law
                 </NavLink>
             </li>
             <li className="nav-item">
-                <NavLink exact activeClassName="active" className="nav-link" to="/Petition">
+                <NavLink exact activeClassName="active"  style={ tabStyles } className="nav-link" to="/Petition">
                     Petition
                 </NavLink>
             </li>
